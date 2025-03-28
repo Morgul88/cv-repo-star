aec7bbbef23cf593545adaf752e3b0689ebf2b20
Henrik <staranderh@gmail.com>
2025-03-28 17:48:03 +0100

    mobil

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,18 +1,29 @@
-import React from 'react';
-import '../style/css/components/Navbar.css'
+import React, { useState } from 'react';
+import '../style/css/components/Navbar.css';
 import { Link, NavLink } from 'react-router-dom';
 
 const Navbar = () => {
+  const [isMobile, setIsMobile] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMobile(!isMobile);
+  };
+
   return (
-    
     <nav className="navbar">
       
-      <ul className="nav-links">
-        
-        <li><NavLink to='/'>Home</NavLink></li>
-        <li><NavLink to='/about'>About</NavLink></li>
-        <li><NavLink href="#">Services</NavLink></li>
-        <li><NavLink href="#">Contact</NavLink></li>
+
+      {/* Hamburger icon */}
+      <div className="hamburger" onClick={toggleMenu}>
+        &#9776;
+      </div>
+
+      {/* Links */}
+      <ul className={`nav-links ${isMobile ? 'active' : ''}`}>
+        <li><NavLink className='nav-bar-link' to='/'>Home</NavLink></li>
+        <li><NavLink className='nav-bar-link' to='/about'>About</NavLink></li>
+        <li><NavLink className='nav-bar-link' to='/services'>Services</NavLink></li>
+        <li><NavLink className='nav-bar-link' to='/contact'>Contact</NavLink></li>
       </ul>
     </nav>
   );
