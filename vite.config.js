bd6ff9752efca194af823e0c899ed84db0c2c11d
Henrik <staranderh@gmail.com>
2025-06-12 20:35:29 +0200

    init3

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,9 @@ import react from '@vitejs/plugin-react'
 
 
 
-// https://vite.dev/config/
+
 export default defineConfig({
-  plugins: [react()],
-  assetsInclude: ['**/*.JPG'], // Lägg till detta
+  plugins: [react()]
+  
 })
 
