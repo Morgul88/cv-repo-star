991d41dd80b86888391e494d1558a89060be960f
Henrik <staranderh@gmail.com>
2025-06-12 17:25:23 +0200

    init2

--- src/components/Wellspect.jsx
+++ src/components/Wellspect.jsx
@@ -6,7 +6,7 @@ const Wellspect = () => {
     <div className='content-text'>
       <Timetag time='2020-2022'/>
       <h2 className='info-title'>Om Wellspect:</h2>
-      <p>Wellspect Healthcare är en global ledare inom medicinsk utrustning, särskilt känd för sina innovativa lösningar som hjälper människor med blås- och tarmdysfunktion. Företaget har över 1000 anställda världen över och strävar efter att bli klimatneutralt. Med produkter som LoFric® och Navina™, har Wellspect förbättrat livskvaliteten för patienter som lider av kronisk förstoppning och urinvägsproblem.</p>
+      <p> Wellspect Healthcare är en global ledare inom medicinsk utrustning, särskilt känd för sina innovativa lösningar som hjälper människor med blås- och tarmdysfunktion. Företaget har över 1000 anställda världen över och strävar efter att bli klimatneutralt. Med produkter som LoFric® och Navina™, har Wellspect förbättrat livskvaliteten för patienter som lider av kronisk förstoppning och urinvägsproblem.</p>
 
       <p>Wellspect har sitt huvudkontor i Mölndal, och som en del av Dentsply Sirona fortsätter de att driva innovationer inom medicinsk teknologi, vilket gör deras produkter och lösningar centrala för en bättre hälsa globalt.</p>
       <h3>Min Roll som Underhållstekniker på Wellspect</h3>
